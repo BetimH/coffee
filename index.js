7b706ea0e3cc2787a0847ddebd38c7aafb68b07c
web <103862044+BetimH@users.noreply.github.com>
2025-05-18 04:43:10 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+function openFunction(){
+    document.getElementById("sidebar").style.width = "300px";
+}
+
+function closeFunction(){
+    document.getElementById("sidebar").style.width = "0px";
+}
